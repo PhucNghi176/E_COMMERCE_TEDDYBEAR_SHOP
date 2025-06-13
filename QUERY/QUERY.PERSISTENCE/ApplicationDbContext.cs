141ec0ae290a30c4dfac2d0ce0772623957f9102
Eric <36512385+PhucNghi176@users.noreply.github.com>
2025-06-13 14:26:26 +0700

    feat: Add `Tag` entity DbSet to `ApplicationDbContext`
    The change adds a `DbSet<Tag>` property to the `ApplicationDbContext` class to enable database operations for the `Tag` entity. This was necessary to support persistence layer functionality for tag-related operations in the application. The property is marked as virtual to allow for mocking in unit tests.

--- QUERY/QUERY.PERSISTENCE/ApplicationDbContext.cs
+++ QUERY/QUERY.PERSISTENCE/ApplicationDbContext.cs
@@ -1,6 +1,8 @@
+using CONTRACT.CONTRACT.DOMAIN.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace QUERY.PERSISTENCE;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
+    public virtual DbSet<Tag> Tags { get; set; }
 }
