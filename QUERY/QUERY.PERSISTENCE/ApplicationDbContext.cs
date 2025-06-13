46c870f614595281859a73d73569b985fa05fcc0
Eric <36512385+PhucNghi176@users.noreply.github.com>
2025-06-13 17:12:09 +0700

    feat: Add Product and ProductTag DbSets to ApplicationDbContext
    Add `Product` and `ProductTag` DbSets to `ApplicationDbContext` to support new domain entities. This change enables Entity Framework Core to manage these entities in the database, including creating migrations and performing CRUD operations. The DbSets are marked as virtual to allow for mocking in unit tests.

--- QUERY/QUERY.PERSISTENCE/ApplicationDbContext.cs
+++ QUERY/QUERY.PERSISTENCE/ApplicationDbContext.cs
@@ -5,4 +5,6 @@ namespace QUERY.PERSISTENCE;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
     public virtual DbSet<Tag> Tags { get; set; }
+    public virtual DbSet<Product> Products { get; set; }
+    public virtual DbSet<ProductTag> ProductTags { get; set; }
 }
