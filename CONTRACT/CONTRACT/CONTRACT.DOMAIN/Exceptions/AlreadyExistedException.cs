236fd5174102211bf077bb19f4b5b7a92a2d376f
Eric <36512385+PhucNghi176@users.noreply.github.com>
2025-06-18 16:52:18 +0700

    refactor: Make message parameter nullable in `AlreadyExistedException`
    The `message` parameter in `AlreadyExistedException` abstract class is now nullable to handle cases where a custom message might not be required. This change improves flexibility when throwing the exception while maintaining backward compatibility with existing code.

--- CONTRACT/CONTRACT/CONTRACT.DOMAIN/Exceptions/AlreadyExistedException.cs
+++ CONTRACT/CONTRACT/CONTRACT.DOMAIN/Exceptions/AlreadyExistedException.cs
@@ -1,2 +1,2 @@
 ﻿namespace CONTRACT.CONTRACT.DOMAIN.Exceptions;
-public abstract class AlreadyExistedException(string title, string message) : DomainException(title, message);
+public abstract class AlreadyExistedException(string title, string? message) : DomainException(title, message);
